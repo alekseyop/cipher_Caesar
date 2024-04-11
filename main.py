f2c78b4d1a118ba946434a6d432b4fd2f79345be
Aleksey <oszn2011@yandex.ru>
2024-04-12 01:50:47 +0300

    теперь и русский текст

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def f(word_in, cypher, step):
+def f(word_in, cypher, step_count):
     """
     Функция шифрования и дешифрования
     параметры word_in: текст
     параметры cypher: 1 - кодирование, -1 - декодирование
-    параметры step: шаг шифрования или 0 по длине слова
+    параметры step_count: шаг шифрования или 0 по длине слова
     возвращает: зашифрованный или дешифрованный текст
     """
     word_out = ''
@@ -14,15 +14,15 @@ def f(word_in, cypher, step):
         else:
             if tmp:
                 for b in tmp:
-                    x = (step if step > 0 else len(tmp)) * cypher  # шаг шифрования и дешифрования
+                    x = (step_count if step_count > 0 else len(tmp)) * cypher  # шаг шифрования и дешифрования
                     abc_count = 26 if ord(b) < 123 else 32  # количество букв в алфавите 26 или 32
                     if b.isupper():
-                        abc_code = 65 if ord(b) < 123 else 1040  # коды буквы в английском и русском алфавите
-                        c = (ord(b) - abc_code + x) % abc_count + abc_code  # шифрование и дешифрование букв
+                        abc_code = 65 if ord(b) < 123 else 1040  # код буквы в английском и русском алфавите
+                        character = (ord(b) - abc_code + x) % abc_count + abc_code  # шифрование и дешифрование букв
                     else:
                         abc_code = 97 if ord(b) < 123 else 1072
-                        c = (ord(b) - abc_code + x) % abc_count + abc_code
-                    word_out += chr(c)
+                        character = (ord(b) - abc_code + x) % abc_count + abc_code
+                    word_out += chr(character)
             tmp = ''
             word_out += a
     return word_out[:-1]
