a49b6594265a682994d6f03951829defbd88378a
Aleksey <oszn2011@yandex.ru>
2024-04-12 01:01:21 +0300

    теперь и русский текст

--- main.py
+++ main.py
@@ -14,22 +14,23 @@ def f(word_in, cypher, step):
         else:
             if tmp:
                 for b in tmp:
-                    x = (step if step > 0 else len(tmp)) * cypher # шаг шифрования и дешифрования
+                    x = (step if step > 0 else len(tmp)) * cypher  # шаг шифрования и дешифрования
+                    abc_count = 26 if ord(b) < 123 else 32
                     if b.isupper():
-                        c = (ord(b) - 65 + x) % 26 + 65
+                        abc_code = 65 if ord(b) < 123 else 1040
+                        c = (ord(b) - abc_code + x) % abc_count + abc_code
                     else:
-                        c = (ord(b) - 97 + x) % 26 + 97
+                        abc_code = 97 if ord(b) < 123 else 1072
+                        c = (ord(b) - abc_code + x) % abc_count + abc_code
                     word_out += chr(c)
             tmp = ''
             word_out += a
     return word_out[:-1]
 
-
 q = input('Введите текст: ')
 q1 = int(input('1 - кодирование, -1 - декодирование: '))
 q2 = int(input('Шаг шифрования 0 по длине слова: '))
 print(f(q, q1, q2))
-
 
 # Enter text: Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.
 # 1 - кодирование, -1 - декодирование: -1
