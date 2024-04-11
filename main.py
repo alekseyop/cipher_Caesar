5882ab5adede592eb3916ac5ee5635eaaa13d2af
Aleksey <oszn2011@yandex.ru>
2024-04-11 23:39:35 +0300

    добавленно шифрования и дешифрования и изменение шага только английский язык

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-def f(word_in):
+def f(word_in, cypher, step):
+    """
+    Функция шифрования и дешифрования
+    параметры word_in: текст
+    параметры cypher: 1 - кодирование, -1 - декодирование
+    параметры step: шаг шифрования 0 по длине слова
+    возвращает: зашифрованный или дешифрованный текст
+    """
     word_out = ''
     tmp = ''
-    for a in word_in:
+    for a in word_in + ' ':
         if a.isalpha():
             tmp += a
         else:
             if tmp:
                 for b in tmp:
+                    x = (step if step > 0 else len(tmp)) * cypher # шаг шифрования и дешифрования
                     if b.isupper():
-                        c = (ord(b) - 65 + len(tmp)) % 26 + 65
+                        c = (ord(b) - 65 + x) % 26 + 65
                     else:
-                        c = (ord(b) - 97 + len(tmp)) % 26 + 97
+                        c = (ord(b) - 97 + x) % 26 + 97
                     word_out += chr(c)
             tmp = ''
             word_out += a
-    return word_out
+    return word_out[:-1]
 
 
-print('Sample Input 1:\nDay, mice. "Year" is a mistake!\nSample Output 1:')
-print(f('Day, mice. "Year" is a mistake!'))
-print('------------------------------------')
-print('Sample Input 2:\nmy name is Python!\nSample Output 2:')
-print(f('my name is Python!'))
+q = input('Enter text: ')
+q1 = int(input('1 - кодирование, -1 - декодирование: '))
+q2 = int(input('Шаг шифрования 0 по длине слова: '))
+print(f(q, q1, q2))
+
+
+# Enter text: Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.
+# 1 - кодирование, -1 - декодирование: -1
+# Шаг шифрования 0 по длине слова: 25
+# The grass is always greener on the other side of the fence.
