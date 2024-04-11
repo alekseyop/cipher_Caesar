eb5b4c9c5b4c188884ef356421c0cb3305b8b038
Aleksey <oszn2011@yandex.ru>
2024-04-12 01:19:21 +0300

    теперь и русский текст

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def f(word_in, cypher, step):
     Функция шифрования и дешифрования
     параметры word_in: текст
     параметры cypher: 1 - кодирование, -1 - декодирование
-    параметры step: шаг шифрования 0 по длине слова
+    параметры step: шаг шифрования или 0 по длине слова
     возвращает: зашифрованный или дешифрованный текст
     """
     word_out = ''
@@ -15,10 +15,10 @@ def f(word_in, cypher, step):
             if tmp:
                 for b in tmp:
                     x = (step if step > 0 else len(tmp)) * cypher  # шаг шифрования и дешифрования
-                    abc_count = 26 if ord(b) < 123 else 32
+                    abc_count = 26 if ord(b) < 123 else 32  # количество букв в алфавите 26 или 32
                     if b.isupper():
-                        abc_code = 65 if ord(b) < 123 else 1040
-                        c = (ord(b) - abc_code + x) % abc_count + abc_code
+                        abc_code = 65 if ord(b) < 123 else 1040  # коды буквы в английском и русском алфавите
+                        c = (ord(b) - abc_code + x) % abc_count + abc_code  # шифрование и дешифрование букв
                     else:
                         abc_code = 97 if ord(b) < 123 else 1072
                         c = (ord(b) - abc_code + x) % abc_count + abc_code
@@ -27,12 +27,13 @@ def f(word_in, cypher, step):
             word_out += a
     return word_out[:-1]
 
+
 q = input('Введите текст: ')
 q1 = int(input('1 - кодирование, -1 - декодирование: '))
-q2 = int(input('Шаг шифрования 0 по длине слова: '))
+q2 = int(input('Целое число, шаг шифрования или 0 по длине слова: '))
 print(f(q, q1, q2))
 
-# Enter text: Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.
-# 1 - кодирование, -1 - декодирование: -1
-# Шаг шифрования 0 по длине слова: 25
-# The grass is always greener on the other side of the fence.
+# Введите текст: АA - первая буква кириллица вторая буква латиница
+# 1 - кодирование, -1 - декодирование: 1
+# Целое число, шаг шифрования или 0 по длине слова: 1
+# БB
